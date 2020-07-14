1c80d1a31659de9e41e5923c70a85af9b3c7b89d
Haeri <Haeri@users.noreply.github.com>
2020-07-15 01:17:53 +0200

    remove apt installs

---
+++ src/color.cpp
@@ -0,0 +1,25 @@
+#include "elemd/color.hpp"
+
+namespace elemd
+{
+    uint8_t Color::r()
+    {
+        return _r;
+    }
+
+    uint8_t Color::g()
+    {
+        return _g;
+    }
+
+    uint8_t Color::b()
+    {
+        return _b;
+    }
+
+    uint8_t Color::a()
+    {
+        return _a;
+    }
+
+} // namespace elemd
