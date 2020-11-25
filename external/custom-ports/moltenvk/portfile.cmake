bf8e0231937a581180f4402d8176cfaaf89d0246
Haeri <Haeri@users.noreply.github.com>
2020-11-25 16:30:17 +0100

    this will take a moment

--- external/custom-ports/moltenvk/portfile.cmake
+++ external/custom-ports/moltenvk/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_fail_port_install(ON_TARGET "Windows" "Linux")
 
 vcpkg_download_distfile(ARCHIVE
-	URLS https://github.com/KhronosGroup/MoltenVK/archive/ce85a96d8041b208e6f32898912b217957019b5a.zip
-	FILENAME MoltenVK.zip
+    URLS https://github.com/KhronosGroup/MoltenVK/archive/ce85a96d8041b208e6f32898912b217957019b5a.zip
+    FILENAME MoltenVK.zip
     SHA512 2a6ffcf504985b47529200578f3b8b09abeaf32d56097abfe2e35981ed503e66ce2b661c663d2135d2ea5650efb889691b10d57b35e89ff19f30f8eb650aecc7
 )
 
@@ -13,12 +13,9 @@ vcpkg_extract_source_archive_ex(
 
 vcpkg_configure_make(
     SOURCE_PATH ${SOURCE_PATH}
-    #PREFER_NINJA
-    OPTIONS
-        ${OPTIONS}
-        macos
+    #PREFER_NINJA        
 )
 
-vcpkg_install_make()
+vcpkg_install_make(BUILD_TARGET "macos")
 
 #vcpkg_fixup_cmake_targets()
