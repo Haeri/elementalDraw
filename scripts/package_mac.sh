395d3f0d6f1866a804f6f347d266e7328da5a124
Haeri <luka.l@gmx.ch>
2021-01-05 05:14:00 +0100

    cleanup

--- scripts/package_mac.sh
+++ scripts/package_mac.sh
@@ -6,7 +6,7 @@ cd $(dirname "$0")
 cd ..
 if [ "$1" = "-static" ]; then
 	mkdir lib
-	cp ./build/libelemd.a* ./lib/ 	
+	cp ./build/libelemd*.a ./lib/
 
 	# ------ Package Examples ------
 
@@ -15,12 +15,17 @@ if [ "$1" = "-static" ]; then
 	cp -R ./build/examples/hello_world/res ./samples/hello_world
 	cp ./build/examples/hello_world/hello-world ./samples/hello_world
 
+	# Overview
+	mkdir -p samples/overview
+	cp -R ./build/examples/overview/res ./samples/overview
+	cp ./build/examples/overview/overview ./samples/overview
+
 	# Game Of Life
 	mkdir -p samples/game_of_life
 	cp ./build/examples/game_of_life/game-of-life ./samples/game_of_life
 else
 	mkdir bin
-	cp ./build/libelemd.so* ./bin/
+	cp ./build/libelemd*.dylib ./bin/
 fi
 
 err=$?
