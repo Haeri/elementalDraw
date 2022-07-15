49e8ccf3b428fe522d8dc18f513e75fd23f8cb0f
Haeri <luka.l@protonmail.ch>
2022-07-15 21:08:46 +0200

    trying to fix macos

--- src/backends/metal/metal_shared_info.hpp
+++ src/backends/metal/metal_shared_info.hpp
@@ -2,9 +2,11 @@
 #define ELEMD_METAL_SHARED_INFO_HPP
 
 #define NS_PRIVATE_IMPLEMENTATION
-#define CA_PRIVATE_IMPLEMENTATION
 #define MTL_PRIVATE_IMPLEMENTATION
-#include "Metal.hpp"
+#define CA_PRIVATE_IMPLEMENTATION
+
+#include <Metal/Metal.hpp>
+#include <QuartzCore/QuartzCore.hpp>
 
 #include <string>
 
